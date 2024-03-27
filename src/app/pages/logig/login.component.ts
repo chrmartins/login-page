3928adb68f01dfd77e75fca2cdee217ae9ba7234
Christian Martins <cmts2010@gmail.com>
2024-03-26 23:43:49 -0300

    feat: add style

--- src/app/pages/logig/login.component.ts
+++ src/app/pages/logig/login.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { DefaultLoginLayoutComponent } from "../../components/default-login-layout/default-login-layout.component";
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
-    selector: 'app-logig',
-    standalone: true,
-    templateUrl: './login.component.html',
-    styleUrl: './login.component.scss',
-    imports: [DefaultLoginLayoutComponent]
+  selector: 'app-logig',
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  imports: [DefaultLoginLayoutComponent, ReactiveFormsModule],
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
 
+  contructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    });
+  }
 }
